71e2ccf6fdd0151c8fa504a84d1d7ea6313e0061
mamaoqing <luyuna1121@163.com>
2021-01-04 18:00:59 +0800

    init

--- src/main/java/com/mq/pc/pic/pic.java
+++ src/main/java/com/mq/pc/pic/pic.java
@@ -3,9 +3,12 @@ package com.mq.pc.pic;
 import com.mq.pc.PCUtil;
 
 import javax.swing.*;
+import javax.swing.text.Document;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 /**
@@ -15,76 +18,117 @@ import java.io.IOException;
  * @projectName pic
  * @date 2021/1/416:22
  */
-class Listen extends Component implements ActionListener {
+//class Listen extends Component implements ActionListener {
+//
+//    private String path = null;
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        String name = e.getActionCommand();
+//        String s = e.paramString();
+//        System.out.println(s);
+//        if ("选择保存位置".equals(name)) {
+//            JFileChooser jfc = new JFileChooser();
+//            jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+//            //开始选择路径
+//            if (JFileChooser.APPROVE_OPTION == jfc.showOpenDialog(Listen.this)) {
+//                path = jfc.getSelectedFile().getAbsolutePath();
+//                System.out.println(path);
+//            }
+//        } else if ("开始下载".equals(name)) {
+//            if (path == null) {
+//                return;
+//            }
+//
+//            try {
+//                PCUtil.download(100, path);
+//
+//                System.exit(0);
+//            } catch (IOException ioException) {
+//                ioException.printStackTrace();
+//            }
+//        }
+//
+//    }
+//}
 
-    private String path = null;
+public class pic extends Component {
     private static JTextField t2 = new JTextField(12);
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String name=e.getActionCommand();
-        String s = e.paramString();
-        System.out.println(s);
-        if("选择保存位置".equals(name)){
-            JFileChooser jfc = new JFileChooser();
-            jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            //开始选择路径
-            if (JFileChooser.APPROVE_OPTION == jfc.showOpenDialog(Listen.this)) {
-                path = jfc.getSelectedFile().getAbsolutePath();
-            }
-        } else if("开始下载".equals(name)){
-            if(path == null){
-                return;
-            }
+    private static JTextField t1 = new JTextField(12);
+    private static JFrame jf = new JFrame("下载图片");
+    private static JLabel jl = new JLabel("数量");
+    private static JLabel wz = new JLabel("保存位置");
+    private static JButton b1 = new JButton("选择保存位置");
+    private static JButton b2 = new JButton("开始下载");
+    private static String path = null;
+    private static JLabel zt = new JLabel("状态");
+    private static JTextField t3 = new JTextField(26);
 
-            try {
-                PCUtil.download(100,path);
-
-                System.exit(0);
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        }
-
+    public pic() {
+        init();
     }
-}
-public class pic extends Listen {
-    public static void main(String[] args) {
-        JFrame jf=new JFrame("下载图片");
-        JLabel jl=new JLabel("数量");
-        JTextField t2 = new JTextField(12);
-        JButton b1 =new JButton("选择保存位置");
-        JButton b2 =new JButton("开始下载");
-        JPanel jp=new JPanel();
+
+    public void init() {
+
+        JPanel jp = new JPanel();
+        jp.add(wz);
+        jp.add(t1);
         jp.add(jl);
         jp.add(t2);
+        jp.add(zt);
+        jp.add(t3);
+
         jp.add(b1);
         jp.add(b2);
         jf.add(jp);
-        jf.setSize(400, 80);
+        jf.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        jf.setSize(400, 120);
         jf.setLocation(700, 300);
         //显示按钮
         jf.setVisible(true);
-        Listen l=new Listen();
-        b1.addActionListener(l);
-        //按钮登录，退出共享绑定事件监控器
-        b2.addActionListener(l);
-
+//        Listen l = new Listen();
+        jf.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        });
     }
 
-    private void choose() {
-        JFileChooser jfc = new JFileChooser();  //新建一个选择文件夹的类型
-        if (jfc.showOpenDialog(pic.this) == JFileChooser.APPROVE_OPTION) { //开始选择路径
-            System.out.println(jfc.getSelectedFile().getAbsolutePath());//打印出所选择的图片的路径
-
-            //---------------------------把选择的图片显示在面板上----------------------//
-            ImageIcon imageIcon = new ImageIcon(jfc.getSelectedFile()
-                    .getAbsolutePath()); //新建一个ImageIcon，这个ImageIcon等于上面所取路径上的图片
-            JLabel jLabel = new JLabel(imageIcon);//新建一个JLabel，把图片放进去
-            jLabel.setBounds(0, 0, imageIcon.getIconWidth(), imageIcon
-                    .getIconHeight());//设置JLabel的大小
-
-//            panel1.add(jLabel);//把JLabel加进panel1中，显示出来
-        }
+    public void listerner() {
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser jfc = new JFileChooser();
+                jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                if (JFileChooser.APPROVE_OPTION == jfc.showOpenDialog(pic.this)) {
+                    path = jfc.getSelectedFile().getAbsolutePath();
+                    t1.setText(path);
+                }
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                path = t1.getText();
+                String text = t2.getText();
+                int i = Integer.parseInt(text);
+                t3.setText("下载中，请稍等！");
+                if (i > 0 && path != null) {
+                    try {
+                        PCUtil.download(i, path);
+                        t3.setText("下载完成，请到<" + path + ">下查看");
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
+    public static void main(String[] args) {
+        pic pic = new pic();
+        pic.listerner();
+
+    }
 }
